cc554aed1fc12e9da727ca7180154ff89dfbc3c9
ascrazy <i.alexandr.sh@gmail.com>
2012-01-03 12:27:14 +0300

    remove unsupported code

--- my_drawing_area.cpp
+++ my_drawing_area.cpp
@@ -15,25 +15,6 @@ MyDrawingArea::MyDrawingArea(Glib::RefPtr<Gtk::Adjustment> _adj_x) :
     {
         adj_x->signal_value_changed().connect(sigc::mem_fun(*this, &MyDrawingArea::update_view), false);
     }
-// Canvas::Canvas(
-//         Glib::RefPtr<Gtk::Adjustment> adj_y, 
-//         Glib::RefPtr<Gtk::Adjustment> adj_x,
-//         Gtk::ColorButton *color_btn) : 
-//     adjustment_y(adj_y), 
-//     adjustment_x(adj_x),
-//     scale_y(1),
-//     scale_x(1),
-//     color_button(color_btn),
-//     color(new Gdk::Color())
-//     {
-//     color->set_rgb_p(0.999, 0.0, 0.0);
-//     signal_draw().connect(sigc::mem_fun(*this, &Canvas::on_draw), false);
-//     adjustment_y->set_value(scale_y);
-//     adjustment_x->set_value(scale_x);
-//     adjustment_y->signal_value_changed().connect(sigc::mem_fun(*this, &Canvas::update_scale_y), false);
-//     adjustment_x->signal_value_changed().connect(sigc::mem_fun(*this, &Canvas::update_scale_x), false);
-//     color_button->set_color(*color);
-//     color_button->signal_color_set().connect(sigc::mem_fun(*this, &Canvas::update_color), false);
 };
 
 bool MyDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& context)
